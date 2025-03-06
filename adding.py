d736a4fcb4b29acb338f53a14e26375a56afe479
CHETHAN-K-96 <chethan.k@s.amity.edu>
2025-03-06 12:03:37 +0530

    new command was added

--- adding.py
+++ adding.py
@@ -2,3 +2,4 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 sum = num1 + num2
 print("The sum of", num1, "and", num2, "is:", sum)
+print(a-2,b=10,a+b=a+b)
